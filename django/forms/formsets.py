8c6d17df6ad4d2db3237159f2d7e47c02d046f62
sahasrara62 <sahasrara62@gamil.com>
2024-08-31 09:35:08 +0530

    using generator comprehension than list comprehension - exit once if formset is invalid, no need to evaluate all formsets

--- django/forms/formsets.py
+++ django/forms/formsets.py
@@ -575,5 +575,4 @@ def formset_factory(
 
 def all_valid(formsets):
     """Validate every formset and return True if all are valid."""
-    # List comprehension ensures is_valid() is called for all formsets.
-    return all([formset.is_valid() for formset in formsets])
+    return all(formset.is_valid() for formset in formsets)
